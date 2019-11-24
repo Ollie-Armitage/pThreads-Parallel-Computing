2d5b980c7cc06bc6b30ec1c7190991f397a831e3
ollie <oliverrarmitage@gmail.com>
2019-11-24 03:18:19 +0000

    Even for one thread, the averaging isn't always working correctly.

--- main.c
+++ main.c
@@ -12,16 +12,18 @@ struct args{
     int DIMENSIONS;
     int POSITION;
     int ROWS_PER_THREAD;
+    int REMAINDER;
     int ALLOCATED_THREADS;
     double** END_ARRAY;
     double** PREV_ARRAY;
     pthread_t *THREAD_IDS;
 };
 
-pthread_mutex_t LOCK;
+pthread_mutex_t ELEMENT_LOCK;
+pthread_mutex_t COPY_LOCK;
+
 
 double PRECISION = 0;
-int PRECISION_FLAG = 0;
 int MAX_THREADS = 0;
 
 double **generate_random_array(int DIMENSIONS);
@@ -29,9 +31,10 @@ double ** allocate_double_array(int DIMENSIONS);
 double ** solver(double** INPUT_ARRAY, int THREAD_NUM, int DIMENSIONS);
 double average(double UP, double DOWN, double LEFT, double RIGHT);
 void dismantle_array(double** ARRAY, int DIMENSIONS);
+void set_array_edges(double** RANDOM_ARRAY, double** AVERAGED_VALUES, int DIMENSIONS);
 void print_double_array(double **array, int DIMENSIONS);
-void copy_array(double ** FROM_ARRAY, double ** TO_ARRAY, int DIMENSIONS);
-void CHECK_PRECISION(double ** END_ARRAY, double ** PREVIOUS_ARRAY, int DIMENSIONS);
+void copy_array_section(double ** TO_ARRAY, double ** FROM_ARRAY, int FIRST_ROW, int FINAL_ROW, int DIMENSIONS);
+int CHECK_PRECISION(double ** END_ARRAY, double ** PREVIOUS_ARRAY, int DIMENSIONS);
 void* runnable(void* args);
 
 
@@ -41,8 +44,13 @@ int IN_PRECISION();
 int main(int argc, char** argv){
     // Format Args.
 
+    pthread_mutex_init(&ELEMENT_LOCK, NULL);
+    pthread_mutex_init(&COPY_LOCK, NULL);
+
+
     int DIMENSIONS = 0;
     int CORRECTNESS_TESTING = 0;
+
 
     sscanf(argv[1], "%d", &MAX_THREADS);
     sscanf(argv[2], "%d", &DIMENSIONS);
@@ -53,53 +61,43 @@ int main(int argc, char** argv){
         sscanf(argv[4], "%d", &CORRECTNESS_TESTING);
     }
 
+
     // Setup Double Array.
 
     double** INPUT_ARRAY = generate_random_array(DIMENSIONS);
-    double ** SINGLE_THREAD_ARRAY = NULL;
-
-    //print_double_array(INPUT_ARRAY);
-
-
-    if(CORRECTNESS_TESTING == 1){
-        SINGLE_THREAD_ARRAY = allocate_double_array(DIMENSIONS);
-        copy_array(INPUT_ARRAY, SINGLE_THREAD_ARRAY, DIMENSIONS);
-        solver(SINGLE_THREAD_ARRAY, 1, DIMENSIONS);
-        print_double_array(SINGLE_THREAD_ARRAY, DIMENSIONS);
-    }
 
     print_double_array(INPUT_ARRAY, DIMENSIONS);
 
+    double** END_ARRAY = solver(INPUT_ARRAY, MAX_THREADS, DIMENSIONS);
 
-    solver(INPUT_ARRAY, MAX_THREADS, DIMENSIONS);
+    print_double_array(END_ARRAY, DIMENSIONS);
 
 
-    //print_double_array(INPUT_ARRAY);
 
-    if(CORRECTNESS_TESTING == 1){
-        CHECK_PRECISION(SINGLE_THREAD_ARRAY, INPUT_ARRAY, DIMENSIONS);
-
-        if(IN_PRECISION()){
-            printf("In Precision!\n");
-        }
-
-    }
 
     return 0;
 }
 
+// Doesn't need to be locked.
+
 void print_double_array(double **array, int DIMENSIONS){
     for(int i = 0; i < DIMENSIONS; i++){
         for(int j = 0; j < DIMENSIONS; j++){
+            pthread_mutex_lock(&ELEMENT_LOCK);
             printf("%f ", array[i][j]);
+            pthread_mutex_unlock(&ELEMENT_LOCK);
         }
         printf("\n");
     }
     printf("\n\n");
 }
 
+// Doesn't need to be locked.
+
 double ** allocate_double_array(int DIMENSIONS){ double** array = malloc(sizeof(double) * DIMENSIONS);
-    for(int i = 0; i < DIMENSIONS; i++){ array[i] = calloc(DIMENSIONS, sizeof(double) * DIMENSIONS);}
+    for(int i = 0; i < DIMENSIONS; i++){
+        array[i] = calloc(DIMENSIONS, sizeof(double) * DIMENSIONS);
+    }
     return array;
 }
 
@@ -126,19 +124,20 @@ void dismantle_array(double** ARRAY, int DIMENSIONS){
 
 double ** solver(double** PREVIOUS_ARRAY, int THREAD_NUM, int DIMENSIONS){
 
-    pthread_mutex_init(&LOCK, NULL);
 
     pthread_t *THREAD_IDS = malloc(sizeof(pthread_t)*MAX_THREADS);
-
     struct args args;
-    args.THREAD_NUMBER = 1;
+    args.THREAD_NUMBER = 0;
     args.DIMENSIONS = DIMENSIONS;
     args.END_ARRAY = allocate_double_array(DIMENSIONS);
     args.PREV_ARRAY = PREVIOUS_ARRAY;
     args.THREAD_IDS = THREAD_IDS;
     args.POSITION = 1;
     args.ROWS_PER_THREAD = (DIMENSIONS - 2) / THREAD_NUM;
+    args.REMAINDER = (DIMENSIONS - 2) % THREAD_NUM;
     args.ALLOCATED_THREADS = THREAD_NUM;
+
+    set_array_edges(args.PREV_ARRAY, args.END_ARRAY, DIMENSIONS);
 
     runnable(&args);
 
@@ -149,33 +148,26 @@ double average(double UP, double DOWN, double LEFT, double RIGHT){
    return (UP + DOWN + LEFT + RIGHT) / 4;
 }
 
-void UPDATE_PRECISION_FLAG(int UPDATE){
-    PRECISION_FLAG = UPDATE;
-}
-
-void CHECK_PRECISION(double ** END_ARRAY, double ** PREVIOUS_ARRAY, int DIMENSIONS){
+int CHECK_PRECISION(double ** END_ARRAY, double ** PREVIOUS_ARRAY, int DIMENSIONS){
 
     for(int i = 0; i < DIMENSIONS; i++){
         for(int j = 0; j < DIMENSIONS; j++){
             if(PREVIOUS_ARRAY[i][j] - END_ARRAY[i][j] > PRECISION || PREVIOUS_ARRAY[i][j] - END_ARRAY[i][j] < -PRECISION){
-                UPDATE_PRECISION_FLAG(0);
-                return;
+                return 0;
             }
         }
     }
 
-    UPDATE_PRECISION_FLAG(1);
+    return 1;
 
 }
 
-int IN_PRECISION(){
-    return PRECISION_FLAG;
-}
-
-void copy_array(double ** FROM_ARRAY, double ** TO_ARRAY, int DIMENSIONS){
-    for(int i = 0; i < DIMENSIONS; i++){
-        for(int j = 0; j < DIMENSIONS; j++){
+void copy_array_section(double ** TO_ARRAY, double ** FROM_ARRAY, int FIRST_ROW, int FINAL_ROW, int DIMENSIONS){
+    for(int i = FIRST_ROW; i < FINAL_ROW; i++){
+        for(int j = 1; j < DIMENSIONS - 1; j++){
+            pthread_mutex_lock(&ELEMENT_LOCK);
             TO_ARRAY[i][j] = FROM_ARRAY[i][j];
+            pthread_mutex_unlock(&ELEMENT_LOCK);
         }
     }
 }
@@ -202,70 +194,67 @@ void* runnable(void* args){
     int DIMENSIONS = ARGS.DIMENSIONS;
     int THREAD_NUMBER = ARGS.THREAD_NUMBER;
     int ROWS_PER_THREAD = ARGS.ROWS_PER_THREAD;
-    int POSITION = ARGS.POSITION;
+    int FIRST_ROW = ARGS.POSITION;
+    int FINAL_ROW = FIRST_ROW + ROWS_PER_THREAD;
     int ALLOCATED_THREADS = ARGS.ALLOCATED_THREADS;
+    int REMAINDER = ARGS.REMAINDER;
     double ** END_ARRAY = ARGS.END_ARRAY;
     double ** PREVIOUS_ARRAY = ARGS.PREV_ARRAY;
     pthread_t * THREAD_IDS =  ARGS.THREAD_IDS;
-    set_array_edges(PREVIOUS_ARRAY, END_ARRAY, DIMENSIONS);
+    int PRECISION_FLAG = 0;
+
     printf("Entering Thread: %d\n", THREAD_NUMBER);
 
-    if(THREAD_NUMBER + 1 < MAX_THREADS){
+    if(FINAL_ROW < (DIMENSIONS - REMAINDER) - 1){
+
+        // Setup Args for next thread.
+
         ARGS.POSITION = ARGS.POSITION + ROWS_PER_THREAD;
-        pthread_create(&THREAD_IDS[ARGS.THREAD_NUMBER++], NULL, runnable, &ARGS);
+        ARGS.THREAD_NUMBER++;
+
+        pthread_create(&THREAD_IDS[THREAD_NUMBER], NULL, runnable, &ARGS);
     }
 
-    int FIRST_ROW = POSITION;
-    int FINAL_ROW = POSITION + ROWS_PER_THREAD;
 
-    while(!IN_PRECISION()) {
+
+    while(!PRECISION_FLAG) {
 
         for (int ROW = FIRST_ROW; ROW < FINAL_ROW; ROW++) {
             for (int COLUMN = 1; COLUMN < (DIMENSIONS - 1); COLUMN++) {
-                // Critical Section.
-                END_ARRAY[ROW][COLUMN] = average(PREVIOUS_ARRAY[ROW][COLUMN + 1], PREVIOUS_ARRAY[ROW][COLUMN - 1], PREVIOUS_ARRAY[ROW + 1][COLUMN], PREVIOUS_ARRAY[ROW - 1][COLUMN]);
-
+                // Critical Section. Don't need for COLUMN + 1's / - 1's.
+                pthread_mutex_lock(&ELEMENT_LOCK);
+                double reads = average(PREVIOUS_ARRAY[ROW][COLUMN + 1], PREVIOUS_ARRAY[ROW][COLUMN - 1], PREVIOUS_ARRAY[ROW + 1][COLUMN], PREVIOUS_ARRAY[ROW - 1][COLUMN]);
+                pthread_mutex_unlock(&ELEMENT_LOCK);
+                END_ARRAY[ROW][COLUMN] = reads;
             }
         }
 
 
-        if(pthread_equal(pthread_self(), THREAD_IDS[ALLOCATED_THREADS])){
-
-                for(int ROW = FINAL_ROW; ROW < DIMENSIONS - 1; ROW++){
-                    for(int COLUMN = 1; COLUMN < (DIMENSIONS - 1); COLUMN++){
-                        pthread_mutex_lock(&LOCK);
-                        END_ARRAY[ROW][COLUMN] = average(PREVIOUS_ARRAY[ROW][COLUMN + 1], PREVIOUS_ARRAY[ROW][COLUMN - 1], PREVIOUS_ARRAY[ROW + 1][COLUMN], PREVIOUS_ARRAY[ROW - 1][COLUMN]);
-                        pthread_mutex_unlock(&LOCK);
-                    }
-
+        if(THREAD_NUMBER + 1 == ALLOCATED_THREADS){
+            for(int ROW = FINAL_ROW; ROW < DIMENSIONS - 1; ROW++){
+                for(int COLUMN = 1; COLUMN < (DIMENSIONS - 1); COLUMN++){
+                    pthread_mutex_lock(&ELEMENT_LOCK);
+                    double reads = average(PREVIOUS_ARRAY[ROW][COLUMN + 1], PREVIOUS_ARRAY[ROW][COLUMN - 1], PREVIOUS_ARRAY[ROW + 1][COLUMN], PREVIOUS_ARRAY[ROW - 1][COLUMN]);
+                    pthread_mutex_unlock(&ELEMENT_LOCK);
+                    END_ARRAY[ROW][COLUMN] = reads;
                 }
-
+            }
         }
 
-        // TODO: REMAINING ROWS.
 
 
-        pthread_mutex_lock(&LOCK);
-        print_double_array(END_ARRAY, DIMENSIONS);
-        pthread_mutex_unlock(&LOCK);
+        PRECISION_FLAG = CHECK_PRECISION(END_ARRAY, PREVIOUS_ARRAY, DIMENSIONS);
 
-        CHECK_PRECISION(END_ARRAY, PREVIOUS_ARRAY, DIMENSIONS);
-        copy_array(END_ARRAY, PREVIOUS_ARRAY, DIMENSIONS);
 
+        copy_array_section(PREVIOUS_ARRAY, END_ARRAY, FIRST_ROW, FINAL_ROW, DIMENSIONS);
     }
 
-
-    printf("Thread Exiting: %d\n", THREAD_NUMBER);
-
-
-    if(THREAD_NUMBER == 1){
-        pthread_mutex_destroy(&LOCK);
-    }
-    else{
+    if(THREAD_NUMBER + 1 != ALLOCATED_THREADS){
         pthread_join(THREAD_IDS[THREAD_NUMBER], NULL);
     }
 
 
+    printf("Thread Exiting: %d\n", THREAD_NUMBER);
 
 }
 
